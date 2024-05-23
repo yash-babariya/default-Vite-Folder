c6bcb27f233436122c7224f72ab74ed9eaf47666
Yash babariya <162944706+yash-babariya@users.noreply.github.com>
2024-05-22 21:47:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.scss'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
+import './styles/index.scss';
 
 function App() {
   return (
